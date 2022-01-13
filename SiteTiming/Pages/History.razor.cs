a907461232b3eeded41e03a7d7eb4050c4b828df
yordan.todorov@mentormate.com <yordan.todorov@mentormate.com>
2022-01-13 20:00:34 +0200

    history url

--- SiteTiming/Pages/History.razor.cs
+++ SiteTiming/Pages/History.razor.cs
@@ -9,10 +9,28 @@ namespace SiteTiming.Pages
     public partial class History
     {
         [Parameter]
+        [SupplyParameterFromQuery(Name = "url")]
         public string? UrlToGetData { get; set; }
+
+        [Inject]
+        public IJSRuntime JsRuntime { get; set; }
+
+        [Inject]
+        public SiteTimingContext SiteTimingContext { get; set; }
+
+        public List<TimingEntity> Timings { get; set; }
 
         protected override void OnInitialized()
         {
+            var isInitializedProp = JsRuntime.GetType().GetProperties().FirstOrDefault(f => f.Name == "IsInitialized");
+            bool isJsInitialized = (bool)isInitializedProp.GetValue(JsRuntime);
+            if (isJsInitialized == true)
+            {
+                Timings = SiteTimingContext.Timings.Include(s => s.Site).Where(s => s.Site.Name == UrlToGetData)
+                   .OrderByDescending(s => s.DateCreated)
+                   .ToList();
+            }
+
             base.OnInitialized();
         }
     }
